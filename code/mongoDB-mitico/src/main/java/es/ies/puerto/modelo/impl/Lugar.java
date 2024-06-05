625001a30000c2e659e10847ee2aa22741e55b02
mackstm <thelewyntm@gmail.com>
2024-06-05 19:25:01 +0100

    Se agrega la clase Lugar

---
+++ code/mongoDB-mitico/src/main/java/es/ies/puerto/modelo/impl/Lugar.java
@@ -0,0 +1,68 @@
+package es.ies.puerto.modelo.impl;
+
+import java.util.Objects;
+
+public class Lugar {
+    private int id;
+    private String nombre;
+    private String localizacion;
+
+    public Lugar() {
+    }
+
+    public Lugar(int id) {
+        this.id = id;
+    }
+
+    public Lugar(int id, String nombre, String localizacion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.localizacion = localizacion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getLocalizacion() {
+        return localizacion;
+    }
+
+    public void setLocalizacion(String localizacion) {
+        this.localizacion = localizacion;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Lugar lugar = (Lugar) o;
+        return id == lugar.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Lugar{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", localizacion='" + localizacion + '\'' +
+                '}';
+    }
+}
