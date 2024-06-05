29a426427e3c574aca3d19aba529b5076ed807c3
PeterMartEsc <petermartesc@gmail.com>
2024-06-05 19:35:52 +0100

    Se agrega la clase Procedencia

---
+++ code/mongoDB-mitico/src/main/java/es/ies/puerto/modelo/impl/Procedencia.java
@@ -0,0 +1,71 @@
+package es.ies.puerto.modelo.impl;
+
+import java.util.Objects;
+
+public class Procedencia {
+
+    private int id;
+
+    private String nombre;
+
+    private String anios;
+
+    public Procedencia() {
+    }
+
+    public Procedencia(int id) {
+        this.id = id;
+    }
+
+    public Procedencia(int id, String nombre, String anios) {
+        this.id = id;
+        this.nombre = nombre;
+        this.anios = anios;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getAnios() {
+        return anios;
+    }
+
+    public void setAnios(String anios) {
+        this.anios = anios;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Procedencia that = (Procedencia) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Procedencia{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", anios='" + anios + '\'' +
+                '}';
+    }
+}
