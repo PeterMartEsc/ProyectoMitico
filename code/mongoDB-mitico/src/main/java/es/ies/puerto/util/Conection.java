3c396c96ad7a4cf9d104dc36d088b961a4863499
PeterMartEsc <petermartesc@gmail.com>
2024-06-05 18:23:09 +0100

    Se agrega el codigo

---
+++ code/mongoDB-mitico/src/main/java/es/ies/puerto/util/Conection.java
@@ -0,0 +1,26 @@
+package es.ies.puerto.util;
+
+import static com.mongodb.client.model.Filters.eq;
+
+import org.bson.Document;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+public class Conection {
+    public static void main( String[] args ) {
+        // Replace the placeholder with your MongoDB deployment's connection string
+        String uri = "mongodb+srv://PeterMartEsc:eanpeEbewXXNtFud@cluster0.kkjkc0h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            MongoDatabase database = mongoClient.getDatabase("sample_mflix");
+            MongoCollection<Document> collection = database.getCollection("movies");
+            Document doc = collection.find(eq("title", "Back to the Future")).first();
+            if (doc != null) {
+                System.out.println(doc.toJson());
+            } else {
+                System.out.println("No matching documents found.");
+            }
+        }
+    }
+}
---
+++ code/mongoDB-mitico/src/main/java/es/ies/puerto/view/View.java
@@ -0,0 +1,4 @@
+package es.ies.puerto.view;
+
+public class View {
+}
