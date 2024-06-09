7d457d8ccd48ec47a9e5853a1380f3c29b2f6750
PeterMartEsc <petermartesc@gmail.com>
2024-06-09 11:47:25 +0100

    Cambios en la conexion

--- code/mongoDB-mitico/src/main/java/es/ies/puerto/util/Conection.java
+++ code/mongoDB-mitico/src/main/java/es/ies/puerto/util/Conection.java
@@ -13,10 +13,15 @@ public class Conection {
     /*
     public static void main( String[] args ) {
         // Replace the placeholder with your MongoDB deployment's connection string
-        String uri = "mongodb+srv://PeterMartEsc:eanpeEbewXXNtFud@cluster0.kkjkc0h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+        // (Hecho, la info está en resources/data.txt)
+        String uri = "mongodb+srv://petermartesc:J4qyuk2gP6mHttJ8@proyectomitico.0iyfune.mongodb.net/" +
+                "?retryWrites=true&w=majority&appName=ProyectoMitico";
         try (MongoClient mongoClient = MongoClients.create(uri)) {
+            //Cambiar aquí "sample_mflix" por el nombre de la Database nuestra
             MongoDatabase database = mongoClient.getDatabase("sample_mflix");
+            //Cambiar aquí la colección por la de la tabla a la que queramos que se conecte
             MongoCollection<Document> collection = database.getCollection("movies");
+            //Esto para buscar específicamente un registro supongo
             Document doc = collection.find(eq("title", "Back to the Future")).first();
             if (doc != null) {
                 System.out.println(doc.toJson());
@@ -26,4 +31,5 @@ public class Conection {
         }
     }
      */
+
 }
