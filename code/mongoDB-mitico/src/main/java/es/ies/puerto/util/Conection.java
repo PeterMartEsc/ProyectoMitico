1341f9395a3316f1b679b1acece953c17155dbad
PeterMartEsc <petermartesc@gmail.com>
2024-06-11 10:15:08 +0100

    cambios en conexión, sigue sin funcionar

--- code/mongoDB-mitico/src/main/java/es/ies/puerto/util/Conection.java
+++ code/mongoDB-mitico/src/main/java/es/ies/puerto/util/Conection.java
@@ -10,26 +10,26 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 public class Conection {
 
-    /*
     public static void main( String[] args ) {
+        /*
         // Replace the placeholder with your MongoDB deployment's connection string
         // (Hecho, la info está en resources/data.txt)
         String uri = "mongodb+srv://petermartesc:J4qyuk2gP6mHttJ8@proyectomitico.0iyfune.mongodb.net/" +
                 "?retryWrites=true&w=majority&appName=ProyectoMitico";
         try (MongoClient mongoClient = MongoClients.create(uri)) {
             //Cambiar aquí "sample_mflix" por el nombre de la Database nuestra
-            MongoDatabase database = mongoClient.getDatabase("sample_mflix");
+            MongoDatabase database = mongoClient.getDatabase("ProyectoMitico");
             //Cambiar aquí la colección por la de la tabla a la que queramos que se conecte
-            MongoCollection<Document> collection = database.getCollection("movies");
+            MongoCollection<Document> collection = database.getCollection("Dios");
             //Esto para buscar específicamente un registro supongo
-            Document doc = collection.find(eq("title", "Back to the Future")).first();
+            Document doc = collection.find(eq("id", 1)).first();
             if (doc != null) {
                 System.out.println(doc.toJson());
             } else {
                 System.out.println("No matching documents found.");
             }
         }
+         */
     }
-     */
 
 }
